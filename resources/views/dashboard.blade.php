53852a220984c7272a4b2df48743b1b947608a8b
Guillaume <guillaume.briard@gmail.com>
2019-02-06 15:50:15 +0700

    Update products views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,26 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-            <br />
-            <div class="card">
                     <div class="card-header">Products</div>
-    
                     <div class="card-body">
                         <ul>
-                            <li>Product List</li>
-                            <li>Add new product</li>
+                        <li><a href="{{url("/products")}}">Product List</li></li>
+                        <li><a href="{{url("/products/create")}}">Add new product</a></li>
                         </ul>
                     </div>
                 </div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,11 +3,28 @@
 @section('content')
 <h1>Products</h1>
 @if(count($products) >=1)
+    <div class="row">
     @foreach($products as $product)
-    <div class="well">
-        <h3>{{$product->name}}</h3>
-    </div>
+        
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{$product->name}}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Code : {{$product->code}}</h6>
+                    <p class="card-text">
+                            Weight : {{$product->weight}}<br />
+                            Cost : {{$product->cost}}TBH<br />
+                            Price : {{$product->price}}TBH<br />
+                            Stock : {{$product->stock}}
+                    </p>
+                    <!--<a href="#" class="card-link">Edit </a>
+                    <a href="#" class="card-link">Delete</a>-->
+                </div>
+            </div>
+        </div>
+    
     @endforeach
+</div>
 @else
     <p>No posts found</p>
 @endif
